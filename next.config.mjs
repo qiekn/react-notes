0ee52a8183a2fc8ceff2d681d4d35c30172e3f1c
qiekn <qiekndev@gmail.com>
2025-09-25 04:09:10 +0800

    feat: my first component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["files.catbox.moe"],
+  },
+};
 
 export default nextConfig;
---
+++ src/app/(blog)/about/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Profile() {
+  return (
+    <>
+      <h1>yaoyao_qiekn</h1>
+      <Image
+        src="https://files.catbox.moe/3t3jd4.jpeg"
+        alt="yaoyao_qiekn"
+        width={450}
+        height={450}
+      />
+    </>
+  )
+}
